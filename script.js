ca64bfaeeffa3f1bcead9c7bc46b63e6ab8dd78f
zakzinzan123 <zakzinzan123@gmail.com>
2022-12-19 23:29:40 +0800

    Add basic HTML, style and JS

---
+++ script.js
@@ -0,0 +1,12 @@
+let myLibrary = [];
+
+const libraryEl = document.querySelector('#library-container');
+
+function Book(title, author, pages, status) {
+    this.title = title;
+    this.author = author;
+    this.pages = pages;
+    this.status = status;
+}
+
+function addBook() {}
