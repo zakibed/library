f44b2de8811491aefd16d4f85ebc0c9298c48d49
zakzinzan123 <zakzinzan123@gmail.com>
2022-12-22 21:37:06 +0800

    Change status into button and add status changing functionality

--- script.js
+++ script.js
@@ -45,21 +45,27 @@ function Book(title, author, pages, status, coverColor, textColor) {
     this.textColor = textColor;
 }
 
+Book.prototype.changeStatus = function () {
+    this.status =
+        this.status === 'Not read'
+            ? 'Reading'
+            : this.status === 'Reading'
+            ? 'Finished'
+            : 'Not read';
+};
+
 function appendElement(element, parent, className) {
     element.className = className;
     parent.appendChild(element);
 }
 
-function showStatus(icon, prop) {
-    if (prop === 'Not read') icon.className = 'fa-solid fa-circle-exclamation';
-    if (prop === 'Reading') icon.className = 'fa-solid fa-spinner';
-    if (prop === 'Finished') icon.className = 'fa-solid fa-circle-check';
+function showStatus(status) {
+    return status === 'Not read'
+        ? 'fa-solid fa-circle-exclamation'
+        : status === 'Reading'
+        ? 'fa-solid fa-spinner'
+        : 'fa-solid fa-circle-check';
 }
-
-// function deleteBook() {
-//     document.querySelector(`data-book-index-${this.className[this.className.length - 1]}`).remove();
-//     console.log(this.className);
-// }
 
 function toggleForm(visibility, blur, disabled) {
     bookForm.style.display = visibility;
@@ -91,8 +97,6 @@ function addBookToLibrary(event) {
 
     displayBooks();
 
-    console.log(myLibrary);
-
     event.preventDefault();
 }
 
@@ -103,19 +107,13 @@ function displayBooks() {
         const bookContainer = document.createElement('div');
         const bookCover = document.createElement('div');
         const bookInfo = document.createElement('div');
+        const statusIcon = document.createElement('i');
 
         appendElement(bookContainer, libraryEl, 'book-container');
         appendElement(bookCover, bookContainer, 'book-cover');
         appendElement(bookInfo, bookContainer, 'book-info');
 
         bookContainer.dataset.bookIndex = i;
-
-        const statusIcon = document.createElement('i');
-        bookInfo.appendChild(statusIcon);
-
-        // const bookSettings = document.createElement('button');
-        // appendElement(bookSettings, bookInfo, `settings-btn-${i}`);
-        // appendElement(document.createElement('i'), bookSettings, 'fa-solid fa-list');
 
         const deleteBtn = document.createElement('button');
         appendElement(deleteBtn, bookInfo, `delete-btn-${i}`);
@@ -128,29 +126,39 @@ function displayBooks() {
             info.textContent = myLibrary[i][prop];
 
             if (prop === 'status') {
-                bookInfo.appendChild(info);
-            } else if (prop === 'pages') {
-                bookInfo.appendChild(info);
+                const statusBtn = document.createElement('button');
+                statusBtn.dataset.bookProperty = prop;
+                statusBtn.textContent = myLibrary[i][prop];
 
-                const settings = document.createElement('div');
-                appendElement(settings, bookInfo, 'settings');
+                bookInfo.appendChild(statusBtn);
+                statusBtn.appendChild(statusIcon);
             } else if (prop === 'coverColor') {
                 document.querySelector(`[data-book-index='${i}'] .book-cover`).style.background =
                     myLibrary[i][prop];
             } else if (prop === 'textColor') {
                 document.querySelector(`[data-book-index='${i}'] .book-cover`).style.color =
                     myLibrary[i][prop];
-            } else {
+            } else if (prop === 'pages') {
+                bookInfo.appendChild(info);
+            } else if (prop === 'title' || prop === 'author') {
                 bookCover.appendChild(info);
             }
-
-            showStatus(statusIcon, myLibrary[i][prop]);
         }
+
+        statusIcon.className = showStatus(myLibrary[i].status);
 
         document.querySelector(`.delete-btn-${i}`).addEventListener('click', function () {
             document.querySelector(`[data-book-index='${i}']`).remove();
             myLibrary.splice(i, 1);
         });
+
+        document
+            .querySelector(`[data-book-index='${i}'] [data-book-property='status']`)
+            .addEventListener('click', function () {
+                myLibrary[i].changeStatus();
+                this.textContent = myLibrary[i].status;
+                appendElement(document.createElement('i'), this, showStatus(myLibrary[i].status));
+            });
     }
 }
 
