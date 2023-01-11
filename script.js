834da21902f47a9ea03c653b27b73a07a0c0cbc6
zakzinzan123 <zakzinzan123@gmail.com>
2023-01-11 18:24:23 +0800

    Replace old obj constructor and prototype, and refactor code

--- script.js
+++ script.js
@@ -1,9 +1,3 @@
-let myLibrary = [
-    new Book('The Fellowship of the Ring', 'J.R.R. Tolkien', 423, 'Finished', 'brown', 'gold'),
-    new Book('The Two Towers', 'J.R.R. Tolkien', 352, 'Reading', 'darkolivegreen', 'gold'),
-    new Book('The Return of the King', 'J.R.R. Tolkien', 416, 'Not read', 'darkslateblue', 'gold')
-];
-
 const libraryEl = document.querySelector('#library-container');
 const bookForm = document.querySelector('#book-form');
 const addBookBtn = document.querySelector('#add-book-btn');
@@ -27,33 +21,20 @@ class Book {
     }
 
     changeStatus() {
-        if (this.status === 'Not read') {
-            this.status = 'Reading';
-        } else if (this.status === 'Reading') {
-            this.status = 'Finished';
-        } else {
-            this.status = 'Not read';
-        }
+        this.status =
+            this.status === 'Not read'
+                ? 'Reading'
+                : this.status === 'Reading'
+                ? 'Finished'
+                : 'Not read';
     }
 }
 
-function Book(title, author, pages, status, coverColor, textColor) {
-    this.title = title;
-    this.author = author;
-    this.status = status;
-    this.pages = pages;
-    this.coverColor = coverColor;
-    this.textColor = textColor;
-}
-
-Book.prototype.changeStatus = function () {
-    this.status =
-        this.status === 'Not read'
-            ? 'Reading'
-            : this.status === 'Reading'
-            ? 'Finished'
-            : 'Not read';
-};
+let myLibrary = [
+    new Book('The Fellowship of the Ring', 'J.R.R. Tolkien', 423, 'Finished', 'brown', 'gold'),
+    new Book('The Two Towers', 'J.R.R. Tolkien', 352, 'Reading', 'darkolivegreen', 'gold'),
+    new Book('The Return of the King', 'J.R.R. Tolkien', 416, 'Not read', 'darkslateblue', 'gold')
+];
 
 function appendElement(element, parent, className) {
     element.className = className;
@@ -125,32 +106,35 @@ function displayBooks() {
         statusIcon.className = showBookStatus(myLibrary[i].status);
 
         for (let prop in myLibrary[i]) {
+            const cover = document.querySelector(`[data-index='${i}'] .book-cover`);
             const info = document.createElement('p');
+
             info.textContent = myLibrary[i][prop];
 
             if (prop === 'status') {
                 const statusBtn = document.createElement('button');
+
                 statusBtn.className = prop;
                 statusBtn.textContent = myLibrary[i][prop];
 
                 bookInfo.appendChild(statusBtn);
                 statusBtn.appendChild(statusIcon);
-            } else if (prop === 'title' || prop === 'author') {
-                appendElement(info, bookCover, prop);
+            } else if (prop === 'coverColor') {
+                cover.style.background = myLibrary[i][prop];
+            } else if (prop === 'textColor') {
+                cover.style.color = myLibrary[i][prop];
             } else if (prop === 'pages') {
                 appendElement(info, bookInfo, prop);
-            } else if (prop === 'coverColor') {
-                document.querySelector(`[data-index='${i}'] .book-cover`).style.background =
-                    myLibrary[i][prop];
-            } else if (prop === 'textColor') {
-                document.querySelector(`[data-index='${i}'] .book-cover`).style.color =
-                    myLibrary[i][prop];
+            } else {
+                appendElement(info, bookCover, prop);
             }
         }
 
         function removeBook() {
             this.closest('.book-container').remove();
+
             myLibrary.splice(i, 1);
+
             document.querySelectorAll('.book-container').forEach((n) => {
                 for (i in myLibrary) {
                     if (myLibrary[i].title === n.dataset.title) n.dataset.index = i;
@@ -177,8 +161,8 @@ function displayBooks() {
 displayBooks();
 root.className = 'dark';
 
-addBookBtn.addEventListener('click', toggleForm.bind(null, 'block', 'blur(2px)', true));
-removeFormBtn.addEventListener('click', toggleForm.bind(null, 'none', 'blur(0px)', false));
+addBookBtn.addEventListener('click', () => toggleForm('block', 'blur(2px)', true));
+removeFormBtn.addEventListener('click', () => toggleForm('none', 'blur(0px)', false));
 bookForm.addEventListener('submit', addBookToLibrary);
 
 theme.addEventListener('change', () => (root.className = theme.checked ? 'dark' : 'light'));
